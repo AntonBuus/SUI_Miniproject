758d77f6edb6dee7d6549dd42ed0943a740522ab
AntonBuus <antonbuushansen@gmail.com>
2025-04-11 00:10:39 +0200

    Cube selection added

--- Assets/_Anton/Embodied Interaction/DetectCurlAndSelect.cs
+++ Assets/_Anton/Embodied Interaction/DetectCurlAndSelect.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Hands.Samples.Gestures.DebugTools;
 
 public class DetectCurlAndSelect : MonoBehaviour
@@ -10,7 +11,13 @@ public class DetectCurlAndSelect : MonoBehaviour
     public XRFingerShapeDebugBar _ring_FSB;
     public XRFingerShapeDebugBar _pinky_FSB;
 
+    public InputActionProperty _deselectItem;
     public GameObject figdetCube;
+    public GameObject figdetShell;
+    public float _shellMultiplier = 0.5f;
+    public GameObject _placeholderObject;
+    public Collider _triggerCollider;
+
 
     [Header("Multipliers")]
     [Range(0.1f, 0.6f)] public float _scaleMultiplier = 0.3f;
@@ -20,11 +27,40 @@ public class DetectCurlAndSelect : MonoBehaviour
     {
         if (other.CompareTag("FidgetCube"))
         {
-            Debug.Log("Hand detected!");
+            Debug.Log("potential object here");
             // Perform actions when the hand enters the trigger area
+            figdetCube = other.gameObject;
+            figdetShell = figdetCube.gameObject.transform.GetChild(0).gameObject;
+            _triggerCollider.enabled = false;
         }
+        // Debug.Log("something here");
     }
     void Update()
+    {
+        if(_deselectItem.action.WasPressedThisFrame() && figdetCube != _placeholderObject)
+        {
+            figdetCube = _placeholderObject;
+            figdetShell = figdetCube.gameObject.transform.GetChild(0).gameObject;
+            _triggerCollider.enabled = true;
+            
+            Debug.Log("deselecting object: ");
+        }
+        if (figdetCube != _placeholderObject)
+        {
+            ScaleObject();
+            ScaleShell();
+        }
+        
+    }
+    private void ScaleShell()
+    {
+        float posVal = _ring_FSB.valueBar.localScale.x;
+        float invertedPosValue = 1 - posVal;
+        float constrainedScale = invertedPosValue * _shellMultiplier;
+        // float clampedScale = Mathf.Clamp(constrainedScale, 0.1f, 0.2f);
+        figdetShell.transform.localScale = new Vector3(1f + constrainedScale, 1f + constrainedScale, 1f + constrainedScale);
+    }
+    void ScaleObject()
     {
         float invertedIndexValue = 1 - _index_FSB.valueBar.localScale.x;
         figdetCube.transform.localScale = new Vector3(invertedIndexValue*_scaleMultiplier, invertedIndexValue*_scaleMultiplier, invertedIndexValue*_scaleMultiplier);
