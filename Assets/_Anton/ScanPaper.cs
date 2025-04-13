a585f6af4f1cd321d961dedbd1be3c6e89b9277f
AntonBuus <antonbuushansen@gmail.com>
2025-04-13 23:01:16 +0200

    Copying cubes work

--- Assets/_Anton/ScanPaper.cs
+++ Assets/_Anton/ScanPaper.cs
@@ -1,29 +1,28 @@
 using UnityEngine;
-using SystemCollections.Generic;
-using System.Collections;
 using TMPro;
 
 public class ScanPaper : MonoBehaviour
 {
     bool isScanned = false;
-    bool readyToScan = false;
+    bool readyToScan = true;
     public GameObject scanEffectPrefab;
     public GameObject pendingToBeSaved;
     public GameObject confirmScanMenu;
-    public TMPtext statusText = "Scan Paper";
+    public TMP_Text statusText;
     
 
     private void OnTriggerEnter(Collider other) 
     {
-        if (readyToScan && other.CompareTag("PaperToScan"))
+        if (readyToScan && other.CompareTag("Paper"))
         {
             readyToScan = false;
             GameObject scanEffect = Instantiate(scanEffectPrefab, transform.position, Quaternion.identity);
             Destroy(scanEffect, 2f); // Destroy the effect after 2 seconds
             Debug.Log("Paper scanned!");
-            pendingToBeSaved = other.GameObject;
+            pendingToBeSaved = other.gameObject;
             confirmScanMenu.SetActive(true);
         }
+        Debug.Log("Hallo");
     }
     
     public void CopyPaper()
@@ -32,7 +31,16 @@ public class ScanPaper : MonoBehaviour
         {
             // Logic to save the scanned paper
             Debug.Log("Paper saved!");
-            Destroy(pendingToBeSaved);
+            GameObject copiedPaper = Instantiate(pendingToBeSaved, pendingToBeSaved.transform.position, pendingToBeSaved.transform.rotation);
+            copiedPaper.name = pendingToBeSaved.name + "_Copy";
+            copiedPaper.transform.SetParent(null); // Unparent the copied paper
+            Renderer copiedRenderer = copiedPaper.GetComponent<Renderer>();
+            if (copiedRenderer != null)
+            {
+                Material copiedMaterial = copiedRenderer.material;
+                Debug.Log("Copied paper material: " + copiedMaterial.name);
+            }
+            copiedRenderer.material.color = Color.cyan; // Change color to light blue for the copied paper
             pendingToBeSaved = null;
         }
         else
